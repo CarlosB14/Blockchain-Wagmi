1a70383af3fca67e133e6ac9f8d8c2376071af7d
Carlos <carlos.ahoravayslocascas@gmail.com>
2024-07-27 17:46:27 +0200

    Create basic consults

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { useAccount } from "wagmi";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useAccount, useBalance } from "wagmi";
 import HeaderComponent from "./components/HeaderComponent";
+import { useWeb3ModalState } from "@web3modal/wagmi/react";
 
 
 function App() {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const { data: balance } = useBalance({ address: address });
+  const { selectedNetworkId } = useWeb3ModalState();
 
   return (
     <Box bgColor={"gray.700"} w="100vw" h="100vh">
@@ -15,6 +18,24 @@ function App() {
       ) : (
       <>
         <HeaderComponent />
+        <Flex gap={4}>
+          <Text color={"white"} fontSize={"2xl"} p={2}>
+            Address: { address } 
+          </Text>
+          {balance && (
+            <>
+              <Text color={"white"} fontSize={"2xl"} p={2}>
+                Balance: { balance.formatted } 
+              </Text>
+              <Text color={"white"} fontSize={"2xl"} p={2}>
+                Symbol: { balance.symbol } 
+              </Text>
+            </>
+          )}
+          <Text color={"white"} fontSize={"2xl"} p={2}>
+            Network: { selectedNetworkId } 
+          </Text>
+        </Flex>
       </>
       )}
     </Box>
